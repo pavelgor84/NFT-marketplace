1b50251c439f24fed2c7a3e364c1b80f32041428
pavelgor84 <pavelgor@gmail.com>
2023-01-23 21:23:20 +0400

    testing preparations

---
+++ deploy/01-deploy-nft-marketplace.js
@@ -0,0 +1,27 @@
+const { network } = require("hardhat")
+const { developementNetworks, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hh-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    let args = [];
+
+    const nftMarketplace = await deploy("NftMarketplace", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: 1
+    })
+
+    if (!developementNetworks.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(nftMarketplace.address, args)
+    }
+
+    log("----------------------------")
+    module.exports.tags = ["all", "nftmarketplace"]
+
+}
+
