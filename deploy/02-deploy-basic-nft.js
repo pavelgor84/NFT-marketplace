709c266fbefa79b42098fac24b782278c08eae7f
pavelgor84 <pavelgor@gmail.com>
2023-01-23 21:36:00 +0400

    deploy basic nft script

---
+++ deploy/02-deploy-basic-nft.js
@@ -0,0 +1,26 @@
+const { network } = require("hardhat")
+const { developementNetworks, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hh-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    let args = [];
+
+    const basicNft = await deploy("BasicNft", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: 1
+    })
+
+    if (!developementNetworks.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(nftMarketplace.address, args)
+    }
+
+    log("----------------------------")
+    module.exports.tags = ["all", "basicnft"]
+
+}
