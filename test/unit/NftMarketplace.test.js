c236bd322faa0b2595ce28e9a666153c836c72c7
pavelgor84 <pavelgor@gmail.com>
2023-01-25 22:12:30 +0400

    few tests

--- test/unit/NftMarketplace.test.js
+++ test/unit/NftMarketplace.test.js
@@ -1,10 +1,11 @@
 const { assert, expect } = require("chai")
 const { ethers, network, deployments, getNamedAccounts } = require("hardhat")
-//const { developementNetworks } = require("../../helper-hh-config")
+const { developementNetworks } = require("../../helper-hh-config")
 
-describe("NFT Marketplace tests", function () {
+!developementNetworks.includes(network.name) ? describe.skip : describe("NFT Marketplace tests", function () {
     let nftMarketplace, basicNft, deployer, player
     const PRICE = ethers.utils.parseEther("0.1")
+    const NEW_PRICE = ethers.utils.parseEther("0.15")
     const TOKEN_ID = 0
 
     beforeEach(async () => {
@@ -29,6 +30,19 @@ describe("NFT Marketplace tests", function () {
         const deployerProceeds = await nftMarketplace.getProceeds(deployer)
         assert(newOwnerOfNFT.toString() == player.address)
         assert(deployerProceeds.toString() == PRICE.toString())
+    })
+
+    it("Can update a listing", async function () {
+        await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+        await nftMarketplace.updateListing(basicNft.address, TOKEN_ID, NEW_PRICE)
+        const listing = await nftMarketplace.getListing(basicNft.address, TOKEN_ID)
+        assert(listing.price.toString() == NEW_PRICE.toString())
+    })
+
+    it("Deny not an owner of the nft", async function () {
+        const nftMarketplacePlayer = nftMarketplace.connect(player)
+        await basicNft.approve(player.address, TOKEN_ID)
+        await expect(nftMarketplacePlayer.listItem(basicNft.address, TOKEN_ID, PRICE)).to.be.revertedWith("NotOwner")
     })
 
 
